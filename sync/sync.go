42102ac3c481a8480953d8537ea25c739ab23015
Phobia <filipe_msr@hotmail.com>
2023-09-30 15:35:49 -0300

    Sync chapter

---
+++ sync/sync.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"sync"
+)
+
+type Counter struct {
+	//	sync.Mutex you can use this (embedding) but it is a bad practice
+	mu    sync.Mutex
+	value int
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
