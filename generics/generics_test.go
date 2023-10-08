5f09592f888bce291394dcc2221ecf32db9d25d9
Phobia <filipe_msr@hotmail.com>
2023-10-08 14:22:33 -0300

    Generics Chapter

---
+++ generics/generics_test.go
@@ -0,0 +1,52 @@
+package generics_test
+
+import (
+	g "go-with-tests/generics"
+	"testing"
+)
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("Asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("Asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "Hello", "Hello")
+		AssertNotEqual(t, "Hello", "World")
+	})
+}
+
+func TestStack(t *testing.T) {
+	t.Run("Integer stack", func(t *testing.T) {
+		myStackOfInts := new(g.Stack[int])
+
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(123)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+
+	t.Run("String stack", func(t *testing.T) {
+		myStackOfStrings := new(g.Stack[string])
+
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+
+		myStackOfStrings.Push("Hello")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+
+		myStackOfStrings.Push("World")
+		value, _ := myStackOfStrings.Pop()
+		AssertEqual(t, value, "World")
+		value, _ = myStackOfStrings.Pop()
+		AssertEqual(t, value, "Hello")
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+	})
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go-with-tests
 
 go 1.21.1
 
-require github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd // indirect
+require github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd
